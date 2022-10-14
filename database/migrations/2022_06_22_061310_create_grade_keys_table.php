b664d5e11b556943129e5e43064f8ce7290c8e36
hasmom <dhasmom01@gmail.com>
2022-10-14 02:50:50 +0100

    grade keys

--- database/migrations/2022_06_22_061310_create_grade_keys_table.php
+++ database/migrations/2022_06_22_061310_create_grade_keys_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('grade_keys', function (Blueprint $table) {
             $table->id();
             $table->string('school_pid');
+            $table->string('class_param_pid')->nullable();
             $table->string('title');
             $table->string('grade');
             $table->float('grade_point')->nullable();
@@ -24,9 +25,8 @@ return new class extends Migration
             $table->float('max_score')->default(0);
             $table->string('color')->nullable();
             $table->string('pid')->unique();
-            $table->string('class_pid')->nullable();
-            $table->string('arm_pid')->nullable();
-            $table->string('class_param_pid')->nullable();
+            // $table->string('class_pid')->nullable();
+            // $table->string('arm_pid')->nullable();
             $table->timestamps();
         });
     }
