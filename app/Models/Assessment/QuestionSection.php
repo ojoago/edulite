92056d5b2075d905437a30f5ae07355b857edd8a
hasmom <dhasmom01@gmail.com>
2023-07-30 01:14:08 +0100

    added question model and migration

---
+++ app/Models/Assessment/QuestionSection.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Assessment;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class QuestionSection extends Model
+{
+    use HasFactory;
+}
