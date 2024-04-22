2178c539a8cadf020a0fb916d26672f238a733be
hassanmomoh <163112395+hassanmomoh@users.noreply.github.com>
2024-04-22 05:41:56 +0100

    stop setup reminder

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('seasonal:greeting')->everyMinute();
         $schedule->command('birthday:greeting')->daily()->runInBackground();
-        $schedule->command('setup:reminder')->weekdays()->at('10:30')->runInBackground();
+        // $schedule->command('setup:reminder')->weekdays()->at('10:30')->runInBackground();
         $schedule->command('custom:greeting')->yearlyOn(1, 17, '10:45')->runInBackground();
         $schedule->command('newmonth:greeting')->monthly()->runInBackground();
         $schedule->command('seasonal:greeting')->yearlyOn(6, 12, '06:45')->runInBackground();
